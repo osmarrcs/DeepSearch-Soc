1ed8c958c92f9183cb04b778690d7f889111eb07
osmarrcs <49820857+osmarrcs@users.noreply.github.com>
2026-07-28 17:52:10 +0000

    fix: ignore ssl cert during db bootstrap

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     ssl: /localhost|127\.0\.0\.1/.test(process.env.DATABASE_URL)
       ? false
       : { rejectUnauthorized: false },
